3e579bd7dd64551d0e19f927d4e206bd1b308dfc
Robbe Louwet <robbe.louwet@student.kuleuven.be>
2023-04-09 13:18:27 +0200

    niks

--- function_app.py
+++ function_app.py
@@ -7,10 +7,3 @@ app = func.FunctionApp()
 
 app.register_functions(bp1)
 app.register_functions(bp2)
-
-
-@app.function_name(name="HttpTrigger1")
-@app.route(route="req")
-def main(req):
-    user = req.params.get('user')
-    return f'Hello, {user}!'
