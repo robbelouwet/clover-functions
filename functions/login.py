e586afe82d02fc20508d1888c090333ae2337d7e
Robbe Louwet <robbe.louwet@hotmail.com>
2023-07-26 16:56:47 +0200

    letterlijk niks

--- functions/login.py
+++ functions/login.py
@@ -11,6 +11,7 @@ bp = func.Blueprint()
 @bp.function_name(name="Log_In")
 @bp.route(route="log-in", methods=["GET"])
 def signup(req: func.HttpRequest) -> func.HttpResponse:
+	
 
 	client_principal = json.loads(base64.b64decode(req.headers.get('x-ms-client-principal')))
 	logging.info(f"client principal:\n{client_principal}")
